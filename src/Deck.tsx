7f033f927307f7dd44504c09480605e2a1d92d7a
EIS\hxv37 <hector.vargas@equifax.com>
2018-12-14 16:16:21 -0300

    remove card from deck added

--- src/Deck.tsx
+++ src/Deck.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import './Deck.css';
 
-class Deck extends React.Component<any, {}> {
+class Deck extends React.Component<any, {cards:any, name:string, format: string}> {
 
     constructor(props: any) {
         super(props);
@@ -17,16 +17,29 @@ class Deck extends React.Component<any, {}> {
             <div className="well">
                 <h4>Deck Builder</h4>
                 <ul>
-                    {this.props.cards.map((card: any) =>
+                    {this.props.cards.map((card: any, index: number) =>
                         <li>
                         {'x'+ card.qty + ' - ' + (card.info.name.length > 15 ? card.info.name.substr(0, 14) + '...' : card.info.name)}
-                        <button className="btn btn-sm btn-secondary add-button">x</button>
+                        <button className="btn btn-sm btn-secondary add-button" onClick={() => this.removeCard(index)}>x</button>
                         </li>
                     )}
                 </ul>
             </div>
         )
     }
+
+    removeCard(index:any) {
+        const deck: any = this.props.cards.splice(index,1);
+        this.updateDeckCards(deck);
+    }
+
+    updateDeckCards(deck: any) {
+        this.setState({
+            cards: deck
+        });
+    }
 }
+
+
 
 export default Deck;
